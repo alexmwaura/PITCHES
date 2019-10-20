c2c5f371de349b6463491be3a97d23b83b89d3c9
alexmwaura <alexmwaura43@gmail.com>
2019-10-21 02:48:46 +0300

    setting configurations for SQLAlchemy

---
+++ config.py
@@ -0,0 +1,49 @@
+import os
+
+class Config:
+    '''   
+    General configuration parent class
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:32915974@localhost/watchlist'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+    
+
+
+
+    
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production':ProdConfig
+}
