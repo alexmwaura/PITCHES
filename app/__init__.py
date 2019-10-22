cfd22569d20b541194843bf12d7d13b2159d1e83
alexmwaura <alexmwaura43@gmail.com>
2019-10-22 12:37:36 +0300

    Add SimpleMde

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_simplemde import SimpleMDE
-from flask_mail import Mail
+# from flask_mail import Mail
 
 
 
@@ -15,7 +15,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-mail = Mail()
+# mail = Mail()
 simple = SimpleMDE()
 
 def create_app(config_name):
@@ -39,14 +39,14 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    mail.init_app(app)
+    # mail.init_app(app)
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
 
     return app
