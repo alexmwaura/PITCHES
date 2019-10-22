2afc7cbd73be88c99c69d0a54cab244e6b2f2f97
alexmwaura <alexmwaura43@gmail.com>
2019-10-22 13:14:22 +0300

    to add all untracked files

--- config.py
+++ config.py
@@ -4,14 +4,16 @@ class Config:
     '''   
     General configuration parent class
     '''
-
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:32915974@localhost/watchlist'
-
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:32915974@localhost/pitches'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    
 
 
     
--- manage.py
+++ manage.py
@@ -1,23 +1,21 @@
-from app import create_app,db
-from app.models import User
-from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
-
+from app import create_app,db
+from flask_script import Manager, Server
+from app.models import User,Role,Coments
 
 app = create_app("development")
 
 manager = Manager(app)
+
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 manager.add_command('server',Server)
-
-
 @manager.command
 def test():
-    """
-    Run the unit tests.
-    """
+    '''
+    Run the unittests.
+    '''
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
@@ -25,9 +23,12 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db, User = User,Role = Role, Coments = Coments)
+
+
 
 if __name__ == '__main__':
     manager.run()
+
 
 
