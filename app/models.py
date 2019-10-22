bb2780c6470695bf3fb08131aca56bad244897a7
alexmwaura <alexmwaura43@gmail.com>
2019-10-22 03:37:53 +0300

    add new instances,initialize mail,change prefix

--- app/models.py
+++ app/models.py
@@ -1,10 +1,47 @@
-from .import db
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255), unique = True, index = True)
+    email = db.Column(db.String(255),unique = True, index =True)
+    role_id = db.Column(db.Integer,db.foreignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
-        return f'User{self.username}'
+        
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__='roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy = 'dynamic')
+
+
+    def __repr__(self):
+        return f'User{self.name}'       
+
+
+class Review (db.Model):
+    '''
+    Review class to define Review Objects
+    '''
