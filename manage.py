0d078d59139a1c00688f92901c2122ef9c098284
alexmwaura <alexmwaura43@gmail.com>
2019-10-23 18:42:33 +0300

    update

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app import create_app,db
 from flask_script import Manager, Server
 from app.models import User,Pitches,Comments
 
-app = create_app("development")
+app = create_app("production")
 
 manager = Manager(app)
 
